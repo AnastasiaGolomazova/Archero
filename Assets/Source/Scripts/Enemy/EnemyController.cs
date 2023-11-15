ee3bb8386fa3189c57c7f75122867252922aaf8a
AnastasiaGolomazova <66026569+AnastasiaGolomazova@users.noreply.github.com>
2023-11-15 20:39:22 +0700

    Add attack

--- Assets/Source/Scripts/Enemy/EnemyController.cs
+++ Assets/Source/Scripts/Enemy/EnemyController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] private float _speed = 4;
+    [SerializeField] private float _speed = 2;
+
+    [SerializeField] private float _stopRange = 0.3f;
 
     private Rigidbody2D _rigidbody;
 
@@ -12,8 +14,12 @@ public class EnemyController : MonoBehaviour
 
     private Animator _animator;
 
+    private float _lastSpeed;
+
+
     private void Awake()
     {
+        _lastSpeed = _speed;
         _player = GameObject.FindWithTag("Player");
         _rigidbody = this.GetComponent<Rigidbody2D>();
         _animator = this.GetComponent<Animator>();
@@ -21,10 +27,19 @@ public class EnemyController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 direction = (_player.transform.position - this.transform.position).normalized;
+        if(Vector2.Distance(_player.transform.position, _rigidbody.transform.position) <= _stopRange)
+        {
+            _speed = 0;
+            _animator.SetBool("IsMoving", false);
+        }
+        else
+        {
+            _speed = _lastSpeed;
+            Vector2 direction = (_player.transform.position - this.transform.position).normalized;
 
-        _rigidbody.MovePosition((Vector2)this.transform.position + direction * _speed * Time.deltaTime);
+            _rigidbody.MovePosition((Vector2)this.transform.position + direction * _speed * Time.deltaTime);
 
-        _animator.SetBool("IsMoving", true);
+            _animator.SetBool("IsMoving", true);
+        }
     }
 }
--- Assets/Source/Scripts/Player/PlayerController.cs
+++ Assets/Source/Scripts/Player/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,9 +10,20 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private FixedJoystick _joystick;
 
+    [SerializeField] private Button _attackButton;
+
     private Rigidbody2D _rigidbody;
 
     private Animator _animator;
+
+    private bool _isAttack = true;
+
+    [SerializeField] private float _cooldown;
+
+    private void Awake()
+    {
+        _attackButton.onClick.AddListener(Attack);
+    }
 
     private void Start()
     {
@@ -30,10 +42,28 @@ public class PlayerController : MonoBehaviour
         _animator.SetFloat("Horizon", _joystick.Direction.x);
         _animator.SetFloat("Speed", _joystick.Direction.sqrMagnitude);
 
-        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1)
+        if(_joystick.Direction.x > 0 || _joystick.Direction.x < 0)
         {
-            _animator.SetFloat("LastHorizon", Input.GetAxisRaw("Horizontal"));
+            _animator.SetFloat("LastHorizon", _joystick.Direction.x);
         }
+    }
+
+    private void Attack()
+    {
+        if (_isAttack)
+        {
+            _animator.SetTrigger("Attack");
+            StartCoroutine(AttackCooldown());
+        }
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        _isAttack = false;
+
+        yield return new WaitForSeconds(_cooldown);
+
+        _isAttack = true;
     }
 
 }
